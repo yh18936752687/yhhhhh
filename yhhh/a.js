c6779bc97eeb3981fcb89b4572ab95b7fe6ff424
yuhao <43666823+yh18936752687@users.noreply.github.com>
2019-04-12 09:48:11 +0800

    Add files via upload

---
+++ yhhh/a.js
@@ -0,0 +1,6 @@
+function init(){
+var divtag = document.getElementById('amao')
+divtag.setAttribute('style','height:60px;width:200px;background-color:red;margin-left:700px;margin-top:360px;left:0')
+(divtag.getAttribute('style'))
+}
+window.onload=init
