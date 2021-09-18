530719fce8de98e7eba35ec382203633d5ab15f6
yuhao <43666823+yh18936752687@users.noreply.github.com>
2021-09-18 11:02:11 +0800

    Add files via upload

---
+++ a.py
@@ -0,0 +1,19 @@
+import turtle
+first_turtle = turtle.Turtle()
+first_turtle.shape('turtle')
+first_turtle.forward(50)
+first_turtle.right(90)
+first_turtle.forward(50)
+first_turtle.right(90)
+first_turtle.forward(50)
+first_turtle.right(90)
+first_turtle.forward(50)
+first_turtle.right(30)
+first_turtle.forward(50)
+first_turtle.right(30)
+first_turtle.right(30)
+first_turtle.forward(50)
+first_turtle.right(90)
+first_turtle.forward(50)
+
+
